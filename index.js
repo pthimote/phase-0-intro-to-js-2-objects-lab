deb74b1f6e64659efb59ff3c74056b7a3ccf47fe
Paul Thimote <pthimote@gmail.com>
2022-09-12 20:06:26 -0400

    commit tree d9f559bce18a2d2e964c74ae356f2cd49402cddb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "john",
+    streetAddress: "Hoffman"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newobject = {...employee}
+    newobject[key] = value
+    return newobject
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newobject = {...employee}
+    delete newobject[key]
+    return newobject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+};
